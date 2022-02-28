d7d5c02130612c385c3e66b2581c7276f4b70cf6
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-02-28 21:38:02 +0100

    creation repository + mise en place import fiches pdt page accueil

---
+++ front/js/script.js
@@ -0,0 +1,38 @@
+//Mise à jour de la fiche produit de la page d'accueil
+
+//déclaration des variables
+
+let itemElement = document.getElementById('items');
+
+//récupérer les données du catalogue via l'API du backend
+
+fetch('http://localhost:3000/api/products/')
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+
+  //promise pour ajouter les fiches pdts dans la page d'acceuil
+  .then(function (array) {
+    console.log(array);
+    let listOfProducts = '';
+    for (let prod in array) {
+      listOfProducts += `<a href="./product.html?id=${array[prod]._id}">
+            <article>
+              <img
+                src=${array[prod].imageUrl}
+                alt=${array[prod].altTxt}
+              />
+              <h3 class="productName">${array[prod].name}</h3>
+              <p class="productDescription">${array[prod].description}</p>
+            </article>
+          </a>`;
+      itemElement.innerHTML = listOfProducts;
+    }
+  })
+
+  // gestion des erreurs sur chargment data via API
+  .catch(function (error) {
+    alert(`l'erreur suivante est survenue ${error}`);
+  });
